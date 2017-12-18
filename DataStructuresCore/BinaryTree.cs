9a7b7a3c638d7a09eaa753596fae2f86ea7e5a65
oleg <philimonovoleg@gmail.com>
2017-12-19 02:13:36 +0300

    add binary tree blank

---
+++ DataStructuresCore/BinaryTree.cs
@@ -0,0 +1,52 @@
+﻿using DataStructures.Interfaces;
+using System;
+
+namespace DataStructures {
+
+  public class BinaryTree : IBinaryTree, System.Collections.ICollection {
+
+    private readonly object syncRoot = new object();
+
+    public int Count { get; private set; }
+
+    public object SyncRoot => syncRoot;
+
+    public bool IsSynchronized => false;
+
+    public IBinaryTreeNode Root { get; private set; }
+
+    public IBinaryTreeNode Add(object value) {
+      throw new NotImplementedException();
+    }
+
+    public void Clear() => Root = null;
+
+    public bool Contains(object value) {
+      throw new NotImplementedException();
+    }
+
+    public void CopyTo(Array array, int index) {
+      throw new NotImplementedException();
+    }
+
+    public IBinaryTreeNode Find(object value) {
+      throw new NotImplementedException();
+    }
+
+    public System.Collections.IEnumerator GetEnumerator() {
+      throw new NotImplementedException();
+    }
+
+    public void Remove(object value) {
+      throw new NotImplementedException();
+    }
+
+    public void Remove(IBinaryTreeNode value) {
+      throw new NotImplementedException();
+    }
+
+    public object[] ToArray() {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ DataStrucures.Interfaces/IBinaryTree.cs
@@ -0,0 +1,15 @@
+﻿namespace DataStructures.Interfaces {
+  public interface IBinaryTreeNode {
+    IBinaryTreeNode Left { get; }
+    IBinaryTreeNode Right { get; }
+    object Value { get; }
+  }
+
+  public interface IBinaryTree : ICollection {
+    IBinaryTreeNode Root { get; }
+    IBinaryTreeNode Add(object value);
+    void Remove(object value);
+    void Remove(IBinaryTreeNode value);
+    IBinaryTreeNode Find(object value);
+  }
+}
